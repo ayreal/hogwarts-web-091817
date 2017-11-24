bb0aa1e7c24cf87742ca949ca7e7c1690f5dba6d
Ariel Herman <aeh.herman@gmail.com>
2017-11-24 16:48:26 -0500

    checkbox working

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,28 @@ import HogList from "./HogList";
 import hogs from "../porkers_data";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isGreased: false,
+      filter: "none"
+    };
+    this.handleGreasedChange = this.handleGreasedChange.bind(this);
+  }
+
+  handleGreasedChange(isGreased) {
+    this.setState({ isGreased: isGreased });
+    console.log("this pig", this.state.isGreased);
+  }
   render() {
     return (
       <div className="App">
         <Nav />
-        <Filter />
+        <Filter
+          onGreasedChange={this.handleGreasedChange}
+          isGreased={this.state.isGreased}
+          filter={this.state.filter}
+        />
         <HogList hogs={hogs} />
       </div>
     );
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,19 +1,30 @@
 import React, { Component } from "react";
 
 class Filter extends Component {
-  // constructor() {
-  //
-  // }
+  constructor() {
+    super();
+    this.handleGreasedChange = this.handleGreasedChange.bind(this);
+  }
+
+  handleGreasedChange(e) {
+    this.props.onGreasedChange(e.target.checked);
+  }
 
   render() {
+    const isGreased = this.props.isGreased;
     return (
       <div>
         <form>
           <select>
-            <option>Name</option>
-            <option>Weight</option>
+            <option value="none">These Hogs Have No Filter</option>
+            <option value="name">Name</option>
+            <option value="weight">Weight</option>
           </select>
-          <input type="checkbox" />Greased Only?
+          <input
+            type="checkbox"
+            checked={isGreased}
+            onChange={this.handleGreasedChange}
+          />Greased Only?
         </form>
         <p />
         <p />
